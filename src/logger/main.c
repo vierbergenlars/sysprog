8197d987a8eecfc4421e66f660d84360a4eeb2ae
Lars Vierbergen <vierbergenlars@gmail.com>
2015-05-16 16:55:31 +0200

    Update logger to use select

--- src/logger/main.c
+++ src/logger/main.c
@@ -20,15 +20,28 @@ int main(void)
         return EXIT_FAILURE;
     }
     int seq = 0;
+    int inSock = fileno(inFd);
 
+    fd_set master;
+    FD_ZERO(&master);
+    FD_SET(inSock, &master);
     while(1) {
-        char line[1024];
-        if(fgets(line, 1024, inFd) !=NULL) {
-            fprintf(outFd, "%d %d %s", ++seq, (int)time(NULL), line);
-            if(strncmp(line, "shutdown", 8) == 0)
-                break;
-        } else {
-            sleep(1);
+        fd_set fds = master;
+        if(select(inSock+1,&fds, NULL, NULL, NULL) > 0) {
+            char line[1024];
+            ssize_t readBytes = read(inSock, line, 1024);
+try_nextline:
+            for(int i = 0; i < readBytes; i++) {
+                if(line[i] == '\n') {
+                    line[i] = '\0';
+                    fprintf(outFd, "%d %d %s\n", ++seq, (int)time(NULL), line);
+                    if(strncmp(line, "shutdown", 8) == 0)
+                        return EXIT_SUCCESS;
+                    memmove(line, &line[i+1], 1024-i);
+                    readBytes-=i;
+                    goto try_nextline;
+                }
+            }
         }
     }
     fclose(inFd);
