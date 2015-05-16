9de1f6d494b349ec622791f33351fe5a65819a46
Lars Vierbergen <vierbergenlars@gmail.com>
2015-05-16 17:11:12 +0200

    Update logger program to use poll instead of select

--- src/logger/main.c
+++ src/logger/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <sys/select.h>
+#include <poll.h>
 
 void* read_until_newline(FILE*);
 int main(void)
@@ -22,12 +22,19 @@ int main(void)
     int seq = 0;
     int inSock = fileno(inFd);
 
-    fd_set master;
-    FD_ZERO(&master);
-    FD_SET(inSock, &master);
+    struct pollfd master[1];
+
+    master[0].fd = inSock;
+    master[0].events = POLLIN;
+
     while(1) {
-        fd_set fds = master;
-        if(select(inSock+1,&fds, NULL, NULL, NULL) > 0) {
+        if(poll(master, 1, -1) > 0) {
+            if(master[0].revents == POLLHUP) {
+                fflush(outFd);
+                fclose(outFd);
+                fclose(inFd);
+                exit(EXIT_SUCCESS);
+            }
             char line[1024];
             ssize_t readBytes = read(inSock, line, 1024);
 try_nextline:
@@ -35,8 +42,6 @@ try_nextline:
                 if(line[i] == '\n') {
                     line[i] = '\0';
                     fprintf(outFd, "%d %d %s\n", ++seq, (int)time(NULL), line);
-                    if(strncmp(line, "shutdown", 8) == 0)
-                        return EXIT_SUCCESS;
                     memmove(line, &line[i+1], 1024-i);
                     readBytes-=i;
                     goto try_nextline;
@@ -44,8 +49,6 @@ try_nextline:
             }
         }
     }
-    fclose(inFd);
-    fclose(outFd);
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
 }
 
